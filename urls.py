e0c99c4c95ce984e0d977bcefbae500accfd706b
Atul Singh <249340987+atulsingh9@users.noreply.github.com>
2026-08-09 23:44:09 +0530

    sending form data into the defined link

--- urls.py
+++ urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = "monopoly_money"
+
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("dashboard/", views.dashboard, name="dashboard")
 ]
--- views.py
+++ views.py
@@ -3,4 +3,18 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
+    if request.method == "POST":
+        name = request.POST["name"]
+        room_id = request.POST["create_room"]
+        join_room_id = request.POST["join_room"]
+
+        
+
+
+        
+        
+
     return render(request, "monopoly_money/index.html")
+
+def dashboard(request):
+    return render(request, "monopoly_money/dashboard.html")
